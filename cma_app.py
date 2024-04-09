c37befca0f60741b47c50183c62031723d27a283
Bruno Guratti <123127842+brunoguratti@users.noreply.github.com>
2024-04-09 12:47:41 -0400

    improved layout

--- cma_app.py
+++ cma_app.py
@@ -9,7 +9,7 @@ import altair as alt
 # Set the page configuration
 st.set_page_config(page_title='Crossover MA Strategy Analysis', layout='wide')
 
-st.title('Crossover MA Strategy Analysis')
+st.title('📈 Crossover MA Strategy Analysis')
 st.write('This app performs a crossover moving average strategy analysis \
          comparing it with a buy and hold strategy.')
 
